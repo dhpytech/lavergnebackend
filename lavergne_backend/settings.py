e352c28d5e44f261241715eae1cddc2d67844ade
DhPyTech-Vn <dh.gentle.man@gmail.com>
2025-06-01 16:05:28 +0700

    Clone

--- lavergne_backend/settings.py
+++ lavergne_backend/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'lavergne_backend.wsgi.application'
-
+WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 if os.path.exists(".env"):
