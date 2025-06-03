2cef40c203a76141a71f2fbc086ba6e9bbf585f4
DhPyTech-Vn <dh.gentle.man@gmail.com>
2025-06-03 10:27:42 +0700

    NEW AGAIN

--- lavergne_backend/settings.py
+++ lavergne_backend/settings.py
@@ -92,8 +92,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [FRONTEND_URL,]
+CORS_ALLOWED_ORIGINS = [
+    "https://lavergnefrontend-production.up.railway.app",
+]
+CORS_ALLOW_CREDENTIALS = True
+
 
 # REST Framework
 REST_FRAMEWORK = {
