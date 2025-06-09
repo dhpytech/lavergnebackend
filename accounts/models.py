7475a749f6de8cd4ad9986cda9f73c64ecfd9934
DhPyTech-Vn <dh.gentle.man@gmail.com>
2025-06-09 16:44:51 +0700

    New backend

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,28 @@ class TenNhanVien(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ItemsCode(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    productType = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class StopTime(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductionProblem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
