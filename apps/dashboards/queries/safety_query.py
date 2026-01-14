88341bac65306e1c20c2f085768e11c9580608d4
dh tech <dh.tech.gentle@gmail.com>
2026-01-14 20:54:30 +0700

    BE04

--- apps/dashboards/queries/safety_query.py
+++ apps/dashboards/queries/safety_query.py
@@ -5,8 +5,7 @@ class SafetyQuery:
     @staticmethod
     def count_events(start, end):
         qs = SafetyTime.objects.filter(
-            safety_date__date__gte=start,
-            safety_date__date__lte=end,
+            date__range=(start, end)
         )
         return {
             "INCIDENT": qs.filter(safety_type="incident").count(),
