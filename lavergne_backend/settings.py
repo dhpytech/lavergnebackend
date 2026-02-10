93b5fb5eab36ee1eb8b46d052bacb7d3b7d3c1ed
dh tech <dh.tech.gentle@gmail.com>
2026-02-10 13:23:51 +0700

    LV01

--- lavergne_backend/settings.py
+++ lavergne_backend/settings.py
@@ -121,3 +121,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+ALLOWED_HOSTS = ["*"]
