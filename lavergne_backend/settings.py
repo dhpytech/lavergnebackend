b28db6b7b0f822ca25393751874a29caad7d99c0
dh tech <dh.tech.gentle@gmail.com>
2026-02-24 18:25:48 +0700

    FIX CORS

--- lavergne_backend/settings.py
+++ lavergne_backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
-# 5. MIDDLEWARE (QUAN TRỌNG: Thứ tự này mới fix được lỗi CORS trong ảnh của bạn)
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -52,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lavergne_backend.urls' # Đã bỏ dấu ngoặc đơn dư thừa
+ROOT_URLCONF = 'lavergne_backend.urls'
 
 TEMPLATES = [
     {
@@ -96,13 +95,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://lavergnefrontend-production.up.railway.app",
+    "https://gunicorn-lavergnebackendwsgi-production.up.railway.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://lavergnefrontend-production.up.railway.app",
     "https://gunicorn-lavergnebackendwsgi-production.up.railway.app",
-    "https://lavergnefrontend-production.up.railway.app",
 ]
 
 # 8. Quốc tế hóa
